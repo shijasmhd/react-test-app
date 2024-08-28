3a5c9ddb7504d3a1f49e7488e1191b913784ba37
Shijas Muhammed <shijas.muhammed@hashcube.com>
2024-08-28 14:20:40 +0530

    Create restaurant cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,85 @@
-import logo from "./assets/192x192.jpg"
-import icon from "./assets/userIcon.svg"
+import { restaurantList } from "./config";
+
+const RestaurantCard = ({cloudinaryImageId, name, cuisines, avgRating}) => {
+  return (
+    <div className="resto-card">
+      <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+        cloudinaryImageId} alt="card image" />
+      <div className="details">
+        <h3>{name}</h3>
+        <h4>{cuisines.join(',')}</h4>
+        <h4>{avgRating + "*"}</h4>
+      </div>
+    </div>
+  );
+}
+
+const CategoriesList = () => {
+  const placeHolder = "Search food or restaurant";
+
+  return (
+    <div className="categories-container">
+      <input type="text" name="search" className="search-bar" placeholder={placeHolder}/>
+    </div>
+  );
+}
+
+const RestaurantList = () => {
+  return (
+    <div className="restaurant-container">
+      {
+        restaurantList.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} {...restaurant.info}/>
+        ))
+      }
+    </div>
+  );
+}
+
+const Body = () => {
+  return (
+    <>
+      <CategoriesList/>
+      <RestaurantList/>
+    </>
+  );
+}
+
+const NavItems = () => {
+  return (
+    <div className="nav-container">
+      <ul className="nav-items">
+        <li className="home">Home</li>
+        <li className="about">About Us</li>
+        <li className="contact-us">Contact Us</li>
+        <li className="cart">Cart</li>
+      </ul>
+    </div>
+  )
+}
 
 const Header = () => {
-  let string = "";
-  let timer;
-
-  const alertFunction = (obj) => {
-    string += obj.key;
-
-    if (timer) {
-      clearTimeout(timer);
-    }
-
-    timer = setTimeout(() => {
-      alert(string);
-      string = "";
-    }, 500);
-  };
-
-  let bg = (
-      <div className="header bg-neutral-600">
-        <img className="header-logo" src={logo} alt="LOGO" />
-        <form className="header-form" action="#" method="get">
-          <input type="text" onChange={alertFunction} name="header-search" id="header-search" placeholder="Enter something" />
-        </form>
-        <img className="header-icon" src={icon} alt="" width={60}/>
-      </div>
-    );
-
-  return bg;
+  return (
+    <div className="header">
+      <img
+        src="./src/assets/logo.png"
+        alt="logo" className="logo" />
+      <select name="location" id="location" className="location">
+        <option value="delhi">Delhi</option>
+        <option value="mumbai">Mumbai</option>
+        <option value="bangalore">Bangalore</option>
+      </select>
+      <NavItems/>
+    </div>
+  )
 }
 
 const App = () => {
   return (
-    <>
+    <div className="app">
       <Header/>
-    </>
+      <Body/>
+    </div>
   );
 }
 
